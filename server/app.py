9d30473b101034c09ddf8def9a8fd2c299d9b61a
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-02-28 19:06:23 +0300

    done

--- server/app.py
+++ server/app.py
@@ -4,5 +4,48 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# default route
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+# route for printing parameter
+@app.route('/print/<string:word>')
+def print_string(word):
+    print(word)
+    return word
+
+# route for counting range of the integer provided
+@app.route('/count/<int:number>')
+def count(number):
+    numbers = '\n'.join(str(n) for n in range(number))
+    return numbers + '\n'
+    
+# Route for performing operations
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    result = None
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return "Error: Division by zero!"
+    elif operation == '%':
+        if num2 != 0:
+            result = num1 % num2
+        else:
+            return "Error : Modulo by zero!"
+    if result is not None:
+        return str(result)  # Convert the result to string for consistency with test expectations
+    else:
+        return "Error: Invalid operation!"
+            
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
